80064a610f23d5516b4edae065d8be66f8dd8cc8
Faralaks <Faralaks@ya.ru>
2022-07-02 14:07:03 +0300

    Add InDocker program argument

--- app/main.go
+++ app/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	extractor "github.com/faralaks/go-vk-expander/app/html_builder/html_extractor"
+	log "github.com/go-pkgz/lgr"
 	"github.com/umputun/go-flags"
-	"log"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var config struct {
 	App struct {
-		Port    string `long:"port" env:"PORT" default:"80" description:"string | Application port"`
-		Address string `long:"address" env:"ADDRESS" default:"0.0.0.0" description:"string | Application web address"`
+		Port     string `long:"port" env:"PORT" default:"80" description:"string | Application port"`
+		Address  string `long:"address" env:"ADDRESS" default:"0.0.0.0" description:"string | Application web address"`
+		Debug    bool   `long:"debug" env:"DEBUG" description:"Debug mode. It provides more output info"`
+		InDocker bool   `long:"indocker" env:"INDOCKER" description:"Is this program runs in docker container"`
 	} `group:"app" namespace:"app" env-namespace:"VKEXP_APP"`
 	OAuth struct {
 		ClientId     string `long:"client_id" env:"CLIENT_ID" required:"true"  description:"string | VK OAuth Client ID"`
@@ -18,20 +23,53 @@ var config struct {
 		RedirectURL  string `long:"redirect_url" env:"REDIRECT_URL" required:"true"  description:"string | VK OAuth full Redirect URL"`
 	} `group:"oauth" namespace:"oauth" env-namespace:"VKEXP_OAUTH"`
 }
+var version = "unknown"
 
 func main() {
-	println("\t ---> Faralaks Vk-Expander starting!")
+	println("\t ---> Faralaks Vk-Expander starting! Version: " + version)
 
 	// Loading configuration
 	p := flags.NewParser(&config, flags.PrintErrors|flags.PassDoubleDash|flags.HelpFlag)
 	if _, err := p.Parse(); err != nil {
 		if err.(*flags.Error).Type != flags.ErrHelp {
-			log.Printf("[ERROR] cli error: %v", err)
+			println("[ERROR] cli error: %v", err)
 		}
 		os.Exit(2)
 	}
-	os.Clearenv() // Clear Environment. Now only this process has this data
+	if config.App.InDocker {
+		os.Clearenv() // Clear Environment. Now only this process has this data
+	}
 
-	println("\t <--- Faralaks Vk-Expander finish!!")
+	setupLog(config.App.Debug)
+	log.Printf("[DEBUG] Log setup Done!")
+	log.Printf("[DEBUG] Config: %+v", config)
 
+	go func() {
+		if x := recover(); x != nil {
+			log.Printf("[WARN] run time panic:\n%v", x)
+			panic(x)
+		}
+
+		// catch signal and invoke graceful termination
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+		<-stop
+		log.Printf("[WARN] interrupt signal")
+	}()
+
+	run("Archive")
+	println("\t <--- Faralaks Vk-Expander finished!!!")
+}
+
+// run: Starts working process. Takes path ro vk messages directory
+func run(path string) {
+	extractor.Run(path)
+}
+
+func setupLog(debug bool) {
+	if debug {
+		log.Setup(log.Debug, log.CallerFile, log.CallerFunc, log.Msec, log.LevelBraces)
+		return
+	}
+	log.Setup(log.Msec, log.LevelBraces)
 }
