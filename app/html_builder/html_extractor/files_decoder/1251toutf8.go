fb813dbebcf58c63c113309e77a9052ca866cb37
Faralaks <Faralaks@ya.ru>
2022-07-03 17:45:58 +0300

    New files_decoder package was added with now only one empty decoder from windows-1251 to utf-8

---
+++ app/html_builder/html_extractor/files_decoder/1251toutf8.go
@@ -0,0 +1,4 @@
+// Package files_decoder
+// Coded by Faralaks https://github.com/Faralaks
+// This package provides functionality  which allows u convert files from one encoding to another.
+package files_decoder
