b84cf37f6b81faa2436bca81d66016484e60ff77
Faralaks <Faralaks@ya.ru>
2022-07-03 18:13:16 +0300

    FileListGetter interface and DecoderWin1251ToUTF8 type  were added

--- app/html_builder/html_extractor/files_decoder/1251toutf8.go
+++ app/html_builder/html_extractor/files_decoder/1251toutf8.go
@@ -2,3 +2,17 @@
 // Coded by Faralaks https://github.com/Faralaks
 // This package provides functionality  which allows u convert files from one encoding to another.
 package files_decoder
+
+type FileListGetter interface {
+	Lock()
+	Unlock()
+	GetFileList() []string
+}
+
+type DecoderWin1251ToUTF8 struct {
+	Dialogs []FileListGetter
+}
+
+func NewDecoderWin1251ToUTF8(dialogs ...FileListGetter) *DecoderWin1251ToUTF8 {
+	return &DecoderWin1251ToUTF8{Dialogs: dialogs}
+}
