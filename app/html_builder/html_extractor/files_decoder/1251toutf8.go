fe796d29ac7381dd547d609c8cf9776e88287f6d
Faralaks <Faralaks@ya.ru>
2022-07-03 21:03:36 +0300

    Implemented decode runner DecoderWin1251ToUTF8

--- app/html_builder/html_extractor/files_decoder/1251toutf8.go
+++ app/html_builder/html_extractor/files_decoder/1251toutf8.go
@@ -3,16 +3,27 @@
 // This package provides functionality  which allows u convert files from one encoding to another.
 package files_decoder
 
-type FileListGetter interface {
-	Lock()
-	Unlock()
-	GetFileList() []string
-}
+import (
+	"context"
+	. "github.com/faralaks/go-vk-expander/app/html_builder/dialog_files"
+)
 
 type DecoderWin1251ToUTF8 struct {
-	Dialogs []FileListGetter
 }
 
-func NewDecoderWin1251ToUTF8(dialogs ...FileListGetter) *DecoderWin1251ToUTF8 {
-	return &DecoderWin1251ToUTF8{Dialogs: dialogs}
+func (d DecoderWin1251ToUTF8) Run(ctx context.Context, dialogsChan chan *Dialog) {
+	for {
+		select {
+		case _ = <-ctx.Done():
+			println("done!")
+			return
+		case dialog := <-dialogsChan:
+			println(dialog.GetFileList()[0])
+
+		}
+	}
+}
+
+func NewDecoderWin1251ToUTF8() *DecoderWin1251ToUTF8 {
+	return &DecoderWin1251ToUTF8{}
 }
